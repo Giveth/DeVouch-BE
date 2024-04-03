4dd86ba4a12accdfec955038a7f4028998770c8a
Amin Latifi <a.latifi.al@gmail.com>
2024-04-03 16:58:46 +0000

    Initial commit

---
+++ src/processor.ts
@@ -0,0 +1,45 @@
+import {assertNotNull} from '@subsquid/util-internal'
+import {lookupArchive} from '@subsquid/archive-registry'
+import {
+    BlockHeader,
+    DataHandlerContext,
+    EvmBatchProcessor,
+    EvmBatchProcessorFields,
+    Log as _Log,
+    Transaction as _Transaction,
+} from '@subsquid/evm-processor'
+
+export const processor = new EvmBatchProcessor()
+    // Lookup archive by the network name in Subsquid registry
+    // See https://docs.subsquid.io/evm-indexing/supported-networks/
+    .setGateway(lookupArchive('eth-mainnet'))
+    // Chain RPC endpoint is required for
+    //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
+    //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
+    .setRpcEndpoint({
+        // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
+        // https://docs.subsquid.io/deploy-squid/env-variables/
+        url: assertNotNull(process.env.RPC_ENDPOINT),
+        // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
+        rateLimit: 10
+    })
+    .setFinalityConfirmation(75)
+    .setFields({
+        transaction: {
+            from: true,
+            value: true,
+            hash: true,
+        },
+    })
+    .setBlockRange({
+        from: 0,
+    })
+    .addTransaction({
+        to: ['0x0000000000000000000000000000000000000000'],
+    })
+
+export type Fields = EvmBatchProcessorFields<typeof processor>
+export type Block = BlockHeader<Fields>
+export type Log = _Log<Fields>
+export type Transaction = _Transaction<Fields>
+export type ProcessorContext<Store> = DataHandlerContext<Store, Fields>
