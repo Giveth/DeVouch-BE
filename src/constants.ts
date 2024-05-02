c09a2c4c8cdda062412b762486b5916202414252
mhmdksh-rekt <mohamad.shehab@rektstudios.com>
2024-05-02 15:27:19 +0300

    Removed unnessescary configs

--- src/constants.ts
+++ src/constants.ts
@@ -7,10 +7,6 @@ export const SCHEMA_CONTRACT_ADDRESS = assertNotNull(
   process.env.SCHEMA_CONTRACT_ADDRESS
 );
 
-export const DB_HOST = assertNotNull(
-  process.env.DB_HOST
-);
-
 export const PROJECT_VERIFY_SCHEMA = assertNotNull(
   process.env.PROJECT_VERIFY_ATTESTATION_SCHEMA
 ).toLocaleLowerCase();
