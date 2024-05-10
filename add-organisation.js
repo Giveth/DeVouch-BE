992e8b1539b4b1895650e7b97e0d5dc65aff42e4
Mitch Oz <divine_comedian@protonmail.com>
2024-05-10 06:40:14 -0600

    add giveth verificationt team

--- add-organisation.js
+++ add-organisation.js
@@ -1,11 +1,11 @@
 const createOrganisationAddMigration =
   require("./db/create-organisation-add-migration").default;
 
-const ORGANISATION_NAME = "Trace"; // Replace with your own organisation name
+const ORGANISATION_NAME = "Giveth Verification Team"; // Replace with your own organisation name
 const SCHEMA_ID =
-  "0x2e22df9a11e06c306ed8f64ca45ceae02efcf8a443371395a78371bc4fb6f722"; // Replace with your own schema id
-const AUTHORIZED_ATTESTOR = "0xF23eA0b5F14afcbe532A1df273F7B233EBe41C78";
-const COLOR = "#FF0000";
+  "0xf63f2a7159ee674aa6fce42196a8bb0605eafcf20c19e91a7eafba8d39fa0404"; // Replace with your own schema id
+const AUTHORIZED_ATTESTOR = "0x826976d7C600d45FB8287CA1d7c76FC8eb732030";
+const COLOR = "#7f64cb";
 
 function main() {
   createOrganisationAddMigration(
