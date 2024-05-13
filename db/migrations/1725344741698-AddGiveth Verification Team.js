45cd17fcd97b94f95b6eacdbfe4b31e071b26dd8
Amin Latifi <a.latifi.al@gmail.com>
2024-05-13 15:18:45 +0330

    Added offset for add organisation migrations

---
+++ db/migrations/1725344741698-AddGiveth Verification Team.js
@@ -0,0 +1,23 @@
+module.exports = class AddGivethVerificationTeam1725344741698 {
+  name = "AddGivethVerificationTeam1725344741698";
+
+  async up(db) {
+    // add organisation with name "Giveth Verification Team" and schema id "0xf63f2a7159ee674aa6fce42196a8bb0605eafcf20c19e91a7eafba8d39fa0404"
+    await db.query(
+      `INSERT INTO "organisation" ("id", "name", "issuer", "color") 
+          VALUES (
+            '0xf63f2a7159ee674aa6fce42196a8bb0605eafcf20c19e91a7eafba8d39fa0404',
+            'Giveth Verification Team',
+            '0x826976d7c600d45fb8287ca1d7c76fc8eb732030',
+            '#7f64cb'
+          )`
+    );
+  }
+
+  async down(db) {
+    // remove organisation with name "Giveth Verification Team" and schema id "0xf63f2a7159ee674aa6fce42196a8bb0605eafcf20c19e91a7eafba8d39fa0404"
+    await db.query(
+      `DELETE FROM "organisation" WHERE "id" = '0xf63f2a7159ee674aa6fce42196a8bb0605eafcf20c19e91a7eafba8d39fa0404'`
+    );
+  }
+};
