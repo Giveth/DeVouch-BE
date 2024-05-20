d89b343dc38fc54d1096bb789fef3178adf3cd95
Cherik <Pourcheriki@gmail.com>
2024-05-20 15:30:51 +0330

    add giveth projects

--- src/features/import-projects/index.ts
+++ src/features/import-projects/index.ts
@@ -1,17 +1,52 @@
 import { Project } from "../../model";
 import { createEntityManager, getDataSource } from "../../helpers/db";
 import { fetchGivethProjects } from "./service";
+import { type EntityManager } from "typeorm";
+
+const updateGivethProject = async () => {
+  const projects = await fetchGivethProjects();
+  console.log("Giveth Project Length", projects.Length);
+  for (const project of projects) {
+    console.log("Project", project);
+    const dataSource = await getDataSource();
+    const exitingProject = await dataSource
+      .getRepository(Project)
+      .createQueryBuilder("project")
+      .where("project.id = :id", { id: `giveth-${project.id}` })
+      .getOne();
+    if (exitingProject) {
+      console.log("Project exists", exitingProject);
+      continue;
+    }
+    const newProject = new Project({
+      id: `giveth-${project.id}`,
+      title: project.title,
+      description: project.descriptionSummary,
+      // image: project.image,
+      // slug: project.slug,
+      projectId: project.id,
+      source: "giveth",
+      totalVouches: 0,
+      totalFlags: 0,
+      totalAttests: 0,
+      lastUpdatedTimestamp: new Date(),
+    });
+    console.log("newProject", newProject);
+    await dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Project)
+      .values([newProject])
+      .execute();
+  }
+};
 
 export const importProjects = async () => {
   try {
-    console.log("importing functions,");
+    console.log("Importing Projects");
     const em = await createEntityManager();
-    const exitingProjects = await em.find(Project);
-    console.log("exitingProjects", exitingProjects);
-
-    const projects = await fetchGivethProjects();
-    console.log("projects", projects);
+    await updateGivethProject();
   } catch (error: any) {
-    console.log("error", error.message);
+    console.log("error", error);
   }
 };
