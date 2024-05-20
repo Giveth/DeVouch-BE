4ff3c13c0ae293b8cdd98bf7f056617c009e9058
Cherik <Pourcheriki@gmail.com>
2024-05-20 16:56:49 +0330

    refactor

--- src/features/import-projects/index.ts
+++ src/features/import-projects/index.ts
@@ -1,73 +1,87 @@
 import { Project } from "../../model";
 import { createEntityManager, getDataSource } from "../../helpers/db";
 import { fetchGivethProjects } from "./service";
-import { type EntityManager } from "typeorm";
+import { DataSource } from "typeorm";
+import { GivethProjectInfo } from "./type";
 
-const updateGivethProject = async () => {
-  const projects = await fetchGivethProjects();
-  console.log("Giveth Project Length", projects.Length);
-  for (const project of projects) {
-    console.log("Project", project);
-    const dataSource = await getDataSource();
-    const exitingProject = await dataSource
-      .getRepository(Project)
-      .createQueryBuilder("project")
-      .where("project.id = :id", { id: `giveth-${project.id}` })
-      .getOne();
-    if (exitingProject) {
-      console.log("Project exists", exitingProject);
-      if (
-        exitingProject?.title === project.title &&
-        exitingProject?.description === project.descriptionSummary &&
-        exitingProject?.slug === project.slug &&
-        exitingProject?.image === project.image
-      ) {
-        continue;
-      }
+const updateOrCreateProject = async (
+  dataSource: DataSource,
+  project: GivethProjectInfo
+) => {
+  const existingProject = await dataSource
+    .getRepository(Project)
+    .createQueryBuilder("project")
+    .where("project.id = :id", { id: `giveth-${project.id}` })
+    .getOne();
+
+  if (existingProject) {
+    console.log("Project exists", existingProject);
+
+    const isUpdated =
+      existingProject.title !== project.title ||
+      existingProject.description !== project.descriptionSummary ||
+      existingProject.slug !== project.slug ||
+      existingProject.image !== project.image;
+
+    if (isUpdated) {
       const updatedProject = new Project({
-        ...exitingProject,
+        ...existingProject,
         title: project.title,
         description: project.descriptionSummary,
         image: project.image,
         slug: project.slug,
         lastUpdatedTimestamp: new Date(),
       });
+
       await dataSource
         .createQueryBuilder()
         .update(Project)
         .set(updatedProject)
         .where("id = :id", { id: `giveth-${project.id}` })
         .execute();
-    } else {
-      const newProject = new Project({
-        id: `giveth-${project.id}`,
-        title: project.title,
-        description: project.descriptionSummary,
-        image: project.image,
-        slug: project.slug,
-        projectId: project.id,
-        source: "giveth",
-        totalVouches: 0,
-        totalFlags: 0,
-        totalAttests: 0,
-        lastUpdatedTimestamp: new Date(),
-      });
-      await dataSource
-        .createQueryBuilder()
-        .insert()
-        .into(Project)
-        .values([newProject])
-        .execute();
     }
+  } else {
+    const newProject = new Project({
+      id: `giveth-${project.id}`,
+      title: project.title,
+      description: project.descriptionSummary,
+      image: project.image,
+      slug: project.slug,
+      projectId: project.id,
+      source: "giveth",
+      totalVouches: 0,
+      totalFlags: 0,
+      totalAttests: 0,
+      lastUpdatedTimestamp: new Date(),
+    });
+
+    await dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Project)
+      .values([newProject])
+      .execute();
+  }
+};
+
+const updateGivethProjects = async () => {
+  const projects = await fetchGivethProjects();
+  console.log("Giveth Project Length", projects.length);
+
+  const dataSource = await getDataSource();
+
+  for (const project of projects) {
+    console.log("Project", project);
+    await updateOrCreateProject(dataSource, project);
   }
 };
 
 export const importProjects = async () => {
   try {
     console.log("Importing Projects");
-    const em = await createEntityManager();
-    await updateGivethProject();
-  } catch (error: any) {
-    console.log("error", error);
+    await createEntityManager();
+    await updateGivethProjects();
+  } catch (error) {
+    console.log("Error", error);
   }
 };
