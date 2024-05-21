2c82074878e67657cad7c626a03da7b481a7b18a
Cherik <Pourcheriki@gmail.com>
2024-05-21 11:28:19 +0330

    set cron job

--- src/features/import-projects/index.ts
+++ src/features/import-projects/index.ts
@@ -1,9 +1,18 @@
+import cron from "node-cron";
 import { fetchAndProcessGivethProjects } from "./giveth/helpers";
+
+const task = async () => {
+  console.log("Importing Projects", new Date());
+  await fetchAndProcessGivethProjects();
+};
 
 export const importProjects = async () => {
   try {
-    console.log("Importing Projects");
-    await fetchAndProcessGivethProjects();
+    console.log("Importing Projects has been scheduled.");
+    cron.schedule("0 0 * * *", task, {
+      scheduled: true,
+      timezone: "UTC",
+    });
   } catch (error) {
     console.log("Error", error);
   }
