aefebdab7071f9f5a299c6482d2a3167392cdf93
Cherik <Pourcheriki@gmail.com>
2024-06-02 16:53:04 +0330

    add givethSourceConfig

--- src/features/import-projects/giveth/constants.ts
+++ src/features/import-projects/giveth/constants.ts
@@ -1,2 +1,11 @@
 export const GIVETH_API_URL =
   process.env.GIVETH_API_URL || "https://mainnet.serve.giveth.io/graphql";
+
+export const givethSourceConfig = {
+  source: "giveth",
+  idField: "id",
+  titleField: "title",
+  descriptionField: "descriptionSummary",
+  slugField: "slug",
+  imageField: "image",
+};
