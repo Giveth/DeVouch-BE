d52feb85fa1218378cbe121bd3fda81bac9f2d18
Cherik <Pourcheriki@gmail.com>
2024-06-02 16:53:13 +0330

    add rpgf3SourceConfig

--- src/features/import-projects/rpgf/constants.ts
+++ src/features/import-projects/rpgf/constants.ts
@@ -1,2 +1,11 @@
 export const RPGF3_API_URL =
   process.env.RPGF3_API_URL || "https://backend.pairwise.vote/mock/projects";
+
+export const rpgf3SourceConfig = {
+  source: "rpgf3",
+  idField: "RPGF3Id",
+  titleField: "name",
+  descriptionField: "impactDescription",
+  slugField: "url",
+  imageField: "image",
+};
