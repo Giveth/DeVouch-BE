79fd50c5b21fe1db9eada556cb9130fb1a2a995c
Cherik <Pourcheriki@gmail.com>
2024-06-02 16:10:19 +0330

    add Rpgf3ProjectInfo

---
+++ src/features/import-projects/rpgf/type.ts
@@ -0,0 +1,14 @@
+export type Rpgf3ProjectInfo = {
+  id: string;
+  name: string;
+  poll_id: string;
+  url: string;
+  impactDescription: string;
+  contributionDescription: string;
+  RPGF3Id: string;
+  parentId: string;
+  image: string;
+  metadataUrl: string;
+  created_at: string;
+  type: string;
+};
