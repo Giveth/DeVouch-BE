a9ee7449c31cd99fed2088d68a6a2cb2216a90a6
Cherik <Pourcheriki@gmail.com>
2024-06-03 10:50:47 +0330

    wrap fetchAndProcessRpgf3Projects with try catch

--- src/features/import-projects/rpgf/index.ts
+++ src/features/import-projects/rpgf/index.ts
@@ -4,12 +4,16 @@ import { rpgf3SourceConfig } from "./constants";
 import { fetchRpgf3Projects } from "./service";
 
 export const fetchAndProcessRpgf3Projects = async () => {
-  const data = await fetchRpgf3Projects();
-  if (!data) return;
-  const dataSource = await getDataSource();
-  if (!dataSource) return;
-  for (const project of data) {
-    if (project.type.toLowerCase() !== "project") continue;
-    await updateOrCreateProject(dataSource, project, rpgf3SourceConfig);
+  try {
+    const data = await fetchRpgf3Projects();
+    if (!data) return;
+    const dataSource = await getDataSource();
+    if (!dataSource) return;
+    for (const project of data) {
+      if (project.type.toLowerCase() !== "project") continue;
+      await updateOrCreateProject(dataSource, project, rpgf3SourceConfig);
+    }
+  } catch (error: any) {
+    console.log("error on fetching RPGF3 projects", error.message);
   }
 };
