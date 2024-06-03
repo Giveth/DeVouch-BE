477a26df0c4db490de3c9ea0ae8305bad9c70432
Cherik <Pourcheriki@gmail.com>
2024-06-03 12:44:59 +0330

    update GitcoinProjectInfo type

--- src/features/import-projects/gitcoin/type.ts
+++ src/features/import-projects/gitcoin/type.ts
@@ -1,7 +1,26 @@
 export type GitcoinProjectInfo = {
   id: string;
+  name: string;
+  applications: GitcoinApplication[];
+  metadata: GitcoinMetadata;
+};
+
+type GitcoinApplication = {
+  roundId: string;
+  chainId: number;
+  id: string;
+};
+
+type GitcoinMetadata = {
+  type: string;
   title: string;
+  logoImg: string;
+  website: string;
+  bannerImg: string;
+  createdAt: number;
+  credentials: {};
   description: string;
-  slug: string;
-  image: string;
+  logoImgData: {};
+  bannerImgData: {};
+  projectTwitter: string;
 };
