648797e9c65268217ceaea6f1018b8387f6a3ef3
Cherik <Pourcheriki@gmail.com>
2024-06-03 13:56:43 +0330

    process projects

--- src/features/import-projects/gitcoin/helpers.ts
+++ src/features/import-projects/gitcoin/helpers.ts
@@ -3,10 +3,32 @@ import type { GitcoinProjectInfo } from "./type";
 import { updateOrCreateProject } from "../helpers";
 import { gitcoinSourceConfig } from "./constants";
 
+const generateGitcoinSlug = (project: GitcoinProjectInfo) => {
+  const application = project.applications[0];
+
+  if (
+    !application ||
+    !application?.roundId ||
+    !application?.chainId ||
+    !application?.id
+  )
+    return ``;
+
+  const { chainId, roundId, id } = application;
+  return `#/round/${chainId}/${roundId}/${id}`;
+};
+
 export const processProjectsBatch = async (
   projectsBatch: GitcoinProjectInfo[]
 ) => {
   for (const project of projectsBatch) {
-    await updateOrCreateProject(project, gitcoinSourceConfig);
+    const processedProject = {
+      id: project.id,
+      title: project.metadata.title,
+      description: project.metadata.description,
+      slug: generateGitcoinSlug(project),
+      image: project.metadata.bannerImg,
+    };
+    await updateOrCreateProject(processedProject, gitcoinSourceConfig);
   }
 };
