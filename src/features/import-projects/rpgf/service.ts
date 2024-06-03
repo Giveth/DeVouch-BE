12b45a9629d4cc48738fa628f6a0041eff88a8d5
Cherik <Pourcheriki@gmail.com>
2024-06-03 10:49:27 +0330

    wrap with try catch

--- src/features/import-projects/rpgf/service.ts
+++ src/features/import-projects/rpgf/service.ts
@@ -2,14 +2,18 @@ import { RPGF3_API_URL } from "./constants";
 import { Rpgf3ProjectInfo } from "./type";
 
 export const fetchRpgf3Projects = async () => {
-  [];
-  const res = await fetch(RPGF3_API_URL, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data: Rpgf3ProjectInfo[] = await res.json();
+  try {
+    const res = await fetch(RPGF3_API_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data: Rpgf3ProjectInfo[] = await res.json();
 
-  return data;
+    return data;
+  } catch (error: any) {
+    console.log("error on fetching fetchRpgf3Projects", error.message);
+    return null;
+  }
 };
