d05113dbd713d0a2bf2172ed5fa3d792fe1843a0
Cherik <Pourcheriki@gmail.com>
2024-06-03 20:51:24 +0330

    update gitcoin query

--- src/features/import-projects/gitcoin/service.ts
+++ src/features/import-projects/gitcoin/service.ts
@@ -10,7 +10,18 @@ export const fetchGitcoinProjectsBatch = async (
     const res = await graphQLRequest(
       GITCOIN_API_URL,
       `query fetchProjects($first: Int = 10, $offset: Int = 10) {
-        projects(first: $first, offset: $offset, orderBy: ID_DESC) {
+        projects(
+          first: $first
+          offset: $offset
+          orderBy: ID_DESC
+          filter: {
+            tags: { contains: "allo-v2" }
+            projectType: { equalTo: CANONICAL }
+            chainId: { in: [1, 137, 10, 324, 42161, 42220, 43114, 534352, 8453] }
+            not: { tags: { contains: "program" } }
+            rounds: { every: { applicationsExist: true } }
+          }
+        ) {
           id
           name
           applications {
@@ -20,7 +31,8 @@ export const fetchGitcoinProjectsBatch = async (
           }
           metadata
         }
-      }`,
+      }
+      `,
       {
         first,
         offset,
