3f48cdc3d11c958daf23e9bbe277dee788713298
Cherik <Pourcheriki@gmail.com>
2024-06-06 02:02:01 +0330

    add Rf4ProjectInfo

---
+++ src/features/import-projects/rf4/type.ts
@@ -0,0 +1,16 @@
+export type Rf4ProjectInfo = {
+  id: string;
+  name: string;
+  displayName: string;
+  description: string;
+  bio: string;
+  address: string;
+  bannerImageUrl: string;
+  profileImageUrl: string;
+  impactCategory: string[];
+  primaryCategory: string;
+  recategorization: string;
+  prelimResult: string;
+  reportReason: string;
+  includedInBallots: number;
+};
