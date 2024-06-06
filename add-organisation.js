c9696ce0c869252854d0bedb11b13d6b649f4aff
Mitch Oz <divine_comedian@protonmail.com>
2024-06-06 14:13:08 -0600

    add honorary mexican

--- add-organisation.js
+++ add-organisation.js
@@ -1,19 +1,11 @@
 const createOrganisationAddMigration =
   require("./db/create-organisation-add-migration").default;
 
-<<<<<<< add-gitcoin-passport
-const ORGANISATION_NAME = "Gitcoin Passport Holder"; // Replace with your own organisation name
+const ORGANISATION_NAME = "Honorary Mexican"; // Replace with your own organisation name
 const SCHEMA_ID =
-  "0x110c216190edf1c2cab264505c0b83437f0caa50298f371ad91a87394d9c55b2"; // Replace with your own schema id
-const AUTHORIZED_ATTESTOR = "0x843829986e895facd330486a61Ebee9E1f1adB1a";
-const COLOR = "#00433b";
-=======
-const ORGANISATION_NAME = "Pizza Lovers"; // Replace with your own organisation name
-const SCHEMA_ID =
-  "0x9224f06bd11d0076dbcb28e4f0c654d78cb071930653510360a3a00518ad1710"; // Replace with your own schema id
+  "0xc80d5c0209976a6c994aa6dc3680f4ad2de2dbc1aa9f46164c251ad9e5e10e09"; // Replace with your own schema id
 const AUTHORIZED_ATTESTOR = "0x826976d7C600d45FB8287CA1d7c76FC8eb732030";
-const COLOR = "#db284a";
->>>>>>> develop
+const COLOR = "#ff0022";
 // staging: eth-sepolia
 // production: optimism-mainnet
 const network = "optimism-mainnet";
