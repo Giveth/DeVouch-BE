1688d62546496b44d503813b99889ac17f596384
Amin Latifi <a.latifi.al@gmail.com>
2024-06-16 12:24:03 +0330

    Revert "removed duplicate AddGitcoin Passport Holder migration"
    This reverts commit 71be6d69a35367b2c92b3c82f4014ff825b33e8f.

---
+++ db/migrations/1728389519384-AddGitcoin Passport Holder.js
@@ -0,0 +1,29 @@
+module.exports = class AddGitcoinPassportHolder1728389519384 {
+    name = "AddGitcoinPassportHolder1728389519384";
+
+    async up(db) {
+        const SQUID_NETWORK = process.env.SQUID_NETWORK || "eth-sepolia";
+        if (SQUID_NETWORK !== "optimism-mainnet") return;
+        // add organisation with name "Gitcoin Passport Holder" and schema id "0x6ab5d34260fca0cfcf0e76e96d439cace6aa7c3c019d7c4580ed52c6845e9c89"
+        await db.query(
+        `INSERT INTO "organisation" ("id", "name", "issuer", "color", "start_block") 
+          VALUES (
+            '0x6ab5d34260fca0cfcf0e76e96d439cace6aa7c3c019d7c4580ed52c6845e9c89',
+            'Gitcoin Passport Holder',
+            '0x843829986e895facd330486a61ebee9e1f1adb1a',
+            '#00433b',
+            108517456
+          )`
+        );
+    }
+    
+    async down(db) {
+        const SQUID_NETWORK = process.env.SQUID_NETWORK || "eth-sepolia";
+        if (SQUID_NETWORK !== "optimism-mainnet") return;
+        // remove organisation with name "Gitcoin Passport Holder" and schema id "0x6ab5d34260fca0cfcf0e76e96d439cace6aa7c3c019d7c4580ed52c6845e9c89"
+        await db.query(
+        `DELETE FROM "organisation" WHERE "id" = '0x6ab5d34260fca0cfcf0e76e96d439cace6aa7c3c019d7c4580ed52c6845e9c89'`
+        );
+    }
+};
+    
