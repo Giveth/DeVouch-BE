97dcf46df57f308c91e23acd863241ad6e5a9c48
Cherik <Pourcheriki@gmail.com>
2024-08-28 11:04:14 +0330

    add types

---
+++ src/features/import-projects/retroList/type.ts
@@ -0,0 +1,18 @@
+export type RoundNumber = 4 | 5;
+
+export type RlProjectInfo = {
+  id: string;
+  name: string;
+  displayName: string;
+  description: string;
+  bio: string;
+  address: string;
+  bannerImageUrl: string;
+  profileImageUrl: string;
+  impactCategory: string[];
+  primaryCategory: string;
+  recategorization: string;
+  prelimResult: string;
+  reportReason: string;
+  includedInBallots: number;
+};
