28f678bebc6466029820d0e5168153affd89b812
Cherik <Pourcheriki@gmail.com>
2024-09-02 12:19:33 +0330

    change issuer address to zero

--- db/migrations/1735226891776-AddNo Affiliation.js
+++ db/migrations/1735226891776-AddNo Affiliation.js
@@ -2,20 +2,21 @@ module.exports = class AddNoAffiliation1735226891776 {
   name = "AddNoAffiliation1735226891776";
 
   async up(db) {
-    // add organisation with name "No Affiliation"
+    // Add organisation with name "No Affiliation"
     await db.query(
       `INSERT INTO "organisation" ("id", "name", "issuer", "color", "start_block") 
-          VALUES (
-            '0x0000000000000000000000000000000000000000000000000000000000000000',
-            'No Affiliation',
-            '0x8f48094a12c8f99d616ae8f3305d5ec73cbaa6b6',
-            '#0049b7',
-          )`
+        VALUES (
+          '0x0000000000000000000000000000000000000000000000000000000000000000',
+          'No Affiliation',
+          '0x0000000000000000000000000000000000000000',
+          '#0049b7',
+          null
+        )`
     );
   }
 
   async down(db) {
-    // remove organisation with name "No Affiliation"
+    // Remove organisation with name "No Affiliation"
     await db.query(
       `DELETE FROM "organisation" WHERE "id" = '0x0000000000000000000000000000000000000000000000000000000000000000'`
     );
