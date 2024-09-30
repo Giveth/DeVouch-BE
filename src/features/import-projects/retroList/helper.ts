1b002af38fa0e05f855fd317e53f49ee661fa13f
Cherik <Pourcheriki@gmail.com>
2024-09-30 13:44:05 +0330

    add manageProjectRemovals

--- src/features/import-projects/retroList/helper.ts
+++ src/features/import-projects/retroList/helper.ts
@@ -7,57 +7,93 @@ export const generateRlUrl = (project: RlProjectInfo) => {
   return `/project/${project.id}`;
 };
 
-export const removeNonExistingProjects = async (
-  processedProjectIds: string[],
+export const manageProjectRemovals = async (
+  newList: RlProjectInfo[] | null,
   sourConfig: SourceConfig,
   round: number // Pass the current round
 ) => {
-  const { source } = sourConfig;
-  const dataSource = await getDataSource();
-  if (!dataSource) {
+  if (newList === null) {
     console.log(
-      `[${new Date().toISOString()}] - ERROR: Failed to remove projects. Data source not found.`
+      `[${new Date().toISOString()}] - ERROR: Failed to manage project removals. New list is null.`
     );
     return;
   }
+  try {
+    const dataSource = await getDataSource();
+    if (!dataSource) {
+      console.log(
+        `[${new Date().toISOString()}] - ERROR: Failed to remove projects. Data source not found.`
+      );
+      return;
+    }
 
-  // Step 1: Fetch existing projects for the source and current round
-  const existingProjectIds = await dataSource
-    .getRepository(Project)
-    .createQueryBuilder("project")
-    .select("project.id")
-    .where("project.source = :source", { source })
-    .andWhere(":round = ANY(project.rfRounds)", { round }) // Only projects in the current round
-    .getMany()
-    .then((projects) => projects.map((proj) => proj.id));
-
-  // Step 2: Identify projects to update (those not processed in the current round)
-  const projectIdsToUpdate = existingProjectIds.filter(
-    (id) => !processedProjectIds.includes(id)
-  );
-
-  if (projectIdsToUpdate.length === 0) {
-    // No projects to update
-    return;
-  }
-
-  // Step 3: Fetch projects to update, including their attests
-  const projectsToUpdate = await dataSource
-    .getRepository(Project)
-    .createQueryBuilder("project")
-    .leftJoinAndSelect("project.attests", "attests")
-    .where("project.id IN (:...ids)", { ids: projectIdsToUpdate })
-    .getMany();
-
-  // Step 4: Process each project individually
-  for (const project of projectsToUpdate) {
-    const updatedRfRounds = (project.rfRounds || []).filter(
-      (rfr) => rfr !== round
+    const shouldKeepProjects = newList.map((project) =>
+      project.prelimResult === "Keep" ? project.id : null
     );
 
-    if (updatedRfRounds.length === 0) {
-      // No rounds remain after removing the current round
-      if (project.imported) {
+    const existingProjectsIds = await dataSource
+      .getRepository(Project)
+      .createQueryBuilder("project")
+      .select("project.id")
+      .where("project.source = :source", { source: sourConfig.source })
+      .andWhere(":round = ANY(project.rfRounds)", { round }) // Only projects in the current round
+      .getMany()
+      .then((projects) => projects.map((proj) => proj.id));
+
+    const projectIdsToManipulate = existingProjectsIds.filter(
+      (id) => !shouldKeepProjects.includes(id)
+    );
+
+    if (projectIdsToManipulate.length === 0) {
+      // No projects to Manipulate
+      return;
+    }
+
+    // Fetch projects to Remove, including their attests
+    const projectsToManipulate = await dataSource
+      .getRepository(Project)
+      .createQueryBuilder("project")
+      .leftJoinAndSelect("project.attests", "attests")
+      .where("project.id IN (:...ids)", { ids: projectIdsToManipulate })
+      .getMany();
+
+    const projectsToRemove: Project[] = [];
+    const projectsToMakeUnImported: Project[] = [];
+
+    projectsToManipulate.forEach(async (project) => {
+      const updatedRfRounds = (project.rfRounds || []).filter(
+        (rfr) => rfr !== round
+      );
+      if (updatedRfRounds.length > 0) {
+        // Update the project with the updated rfRounds
+        try {
+          await dataSource
+            .createQueryBuilder()
+            .update(Project)
+            .set({ rfRounds: updatedRfRounds })
+            .where("id = :id", { id: project.id })
+            .execute();
+
+          console.log(
+            `[${new Date().toISOString()}] - INFO: Project updated with removed round. Project ID: ${project.id}`
+          );
+        } catch (updateError: any) {
+          console.log(
+            `[${new Date().toISOString()}] - ERROR: Failed to update project rfRounds. Project ID: ${project.id}. Error: ${updateError.message}`
+          );
+        }
+      } else {
+        if (project.attests.length > 0) {
+          projectsToMakeUnImported.push(project);
+        } else {
+          projectsToRemove.push(project);
+        }
+      }
+    });
+
+    // Remove projects without attests
+    if (projectsToRemove.length > 0) {
+      projectsToRemove.forEach(async (project) => {
         try {
           await dataSource
             .createQueryBuilder()
@@ -70,46 +106,37 @@ export const removeNonExistingProjects = async (
             `[${new Date().toISOString()}] - INFO: Project Deleted. Project ID: ${project.id}`
           );
         } catch (error: any) {
-          // Mark project as not imported if failed to delete
           console.log(
             `[${new Date().toISOString()}] - ERROR: Failed to delete project. Project ID: ${project.id}. Error: ${error.message}`
           );
-          try {
-            await dataSource
-              .createQueryBuilder()
-              .update(Project)
-              .set({ imported: false, rfRounds: updatedRfRounds })
-              .where("id = :id", { id: project.id })
-              .execute();
-
-            console.log(
-              `[${new Date().toISOString()}] - INFO: Project marked as not imported. Project ID: ${project.id}`
-            );
-          } catch (updateError: any) {
-            console.log(
-              `[${new Date().toISOString()}] - ERROR: Failed to mark project as not imported. Project ID: ${project.id}. Error: ${updateError.message}`
-            );
-          }
         }
-      }
-    } else {
-      // Update the project with the updated rfRounds
-      try {
-        await dataSource
-          .createQueryBuilder()
-          .update(Project)
-          .set({ rfRounds: updatedRfRounds })
-          .where("id = :id", { id: project.id })
-          .execute();
-
-        console.log(
-          `[${new Date().toISOString()}] - INFO: Project updated with removed round. Project ID: ${project.id}`
-        );
-      } catch (updateError: any) {
-        console.log(
-          `[${new Date().toISOString()}] - ERROR: Failed to update project rfRounds. Project ID: ${project.id}. Error: ${updateError.message}`
-        );
-      }
+      });
     }
+
+    // Mark projects without attests as not imported
+    if (projectsToMakeUnImported.length > 0) {
+      projectsToMakeUnImported.forEach(async (project) => {
+        try {
+          await dataSource
+            .createQueryBuilder()
+            .update(Project)
+            .set({ imported: false, rfRounds: [round] })
+            .where("id = :id", { id: project.id })
+            .execute();
+
+          console.log(
+            `[${new Date().toISOString()}] - INFO: Project marked as not imported. Project ID: ${project.id}`
+          );
+        } catch (updateError: any) {
+          console.log(
+            `[${new Date().toISOString()}] - ERROR: Failed to mark project as not imported. Project ID: ${project.id}. Error: ${updateError.message}`
+          );
+        }
+      });
+    }
+  } catch (error: any) {
+    console.log(
+      `[${new Date().toISOString()}] - ERROR: Failed to manage project removals. Error: ${error.message}`
+    );
   }
 };
--- src/features/import-projects/retroList/index.ts
+++ src/features/import-projects/retroList/index.ts
@@ -1,6 +1,6 @@
 import { updateOrCreateProject } from "../helpers";
 import { rlSourceConfig } from "./constants";
-import { generateRlUrl, removeNonExistingProjects } from "./helper";
+import { generateRlUrl, manageProjectRemovals } from "./helper";
 import { fetchRlProjects } from "./service";
 
 export const fetchAndProcessRlProjects = async (round: number) => {
@@ -17,17 +17,11 @@ export const fetchAndProcessRlProjects = async (round: number) => {
         rfRound: round,
       };
 
-      const processedId = await updateOrCreateProject(
-        processedProject,
-        rlSourceConfig
-      );
-      if (processedId) {
-        processedProjectIds.push(processedId);
-      }
+      await updateOrCreateProject(processedProject, rlSourceConfig);
     }
 
     // After processing all new projects, handle projects not in the new dataset for the current round
-    await removeNonExistingProjects(processedProjectIds, rlSourceConfig, round);
+    await manageProjectRemovals(data, rlSourceConfig, round);
   } catch (error: any) {
     console.log("error on fetchAndProcessRlProjects", error.message);
   }
