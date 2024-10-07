c573b2b452a3551f364d3e374163c7c58afef0e6
Cherik <Pourcheriki@gmail.com>
2024-10-07 18:15:48 +0330

    update gitcoin

--- src/features/import-projects/gitcoin/helpers.ts
+++ src/features/import-projects/gitcoin/helpers.ts
@@ -30,6 +30,9 @@ export const processProjectsBatch = async (
       descriptionHtml: description
         ? converter.makeHtml(description)
         : undefined,
+      creationDate: project.metadata.createdAt
+        ? new Date(project.metadata.createdAt * 1000).toISOString() // Convert to ISO 8601
+        : null,
     };
     await updateOrCreateProject(processedProject, gitcoinSourceConfig);
   }
