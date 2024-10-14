8ae4b2161675cc029f3941217346824f6327eb33
Cherik <Pourcheriki@gmail.com>
2024-10-14 18:49:13 +0330

    add working query

--- src/server-extension/types.ts
+++ src/server-extension/types.ts
@@ -1,28 +1,34 @@
-import { Field, ID, Int, ObjectType } from "type-graphql";
+import { Field, ObjectType, ID, Int } from "type-graphql";
+
+@ObjectType()
+export class OrganisationType {
+  @Field(() => ID)
+  id!: string;
+
+  @Field()
+  name!: string;
+}
+
+@ObjectType()
+export class OrganisationProjectType {
+  @Field(() => Boolean)
+  vouch!: boolean;
+
+  @Field(() => Int)
+  count!: number;
+
+  @Field(() => OrganisationType)
+  organisation!: OrganisationType;
+}
 
 @ObjectType()
 export class ProjectType {
   @Field(() => ID)
   id!: string;
 
-  @Field({ nullable: true }) // Allowing null or undefined
-  title?: string;
+  @Field(() => String, { nullable: true }) // Explicitly define the GraphQL type for title
+  title?: string | null;
 
-  @Field({ nullable: true })
-  description?: string;
-
-  @Field(() => Int)
-  totalVouches!: number;
-
-  @Field(() => Int)
-  totalFlags!: number;
-
-  @Field(() => Int)
-  totalAttests!: number;
-
-  @Field({ nullable: true })
-  url?: string;
-
-  @Field({ nullable: true })
-  image?: string;
+  @Field(() => [OrganisationProjectType])
+  attestedOrganisations!: OrganisationProjectType[];
 }
