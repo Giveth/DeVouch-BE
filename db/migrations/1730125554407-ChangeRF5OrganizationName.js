d0eb4fc2c12aa153b860065b886bb42ccd9f5b7d
Cherik <Pourcheriki@gmail.com>
2024-10-28 18:08:21 +0330

    fix typo

--- db/migrations/1730125554407-ChangeRF5OrganizationName.js
+++ db/migrations/1730125554407-ChangeRF5OrganizationName.js
@@ -2,7 +2,7 @@ module.exports = class ChangeRF5OrganizationName1730125554407 {
   name = "ChangeRF5OrganizationName1730125554407";
 
   async up(db) {
-    // Update the organisation name from 'RF 5 Voter' to ''RF 5 or 6 Voter' for the specific ID
+    // Update the organisation name from 'RF 5 Voter' to 'RF 5 or 6 Voter' for the specific ID
     await db.query(`
       UPDATE "organisation" 
       SET "name" = 'RF 5 or 6 Voter' 
@@ -12,12 +12,12 @@ module.exports = class ChangeRF5OrganizationName1730125554407 {
   }
 
   async down(db) {
-    // Revert the organisation name from ''RF 5 or 6 Voter' back to 'RF 5 Voter' for the specific ID
+    // Revert the organisation name from 'RF 5 or 6 Voter' back to 'RF 5 Voter' for the specific ID
     await db.query(`
       UPDATE "organisation" 
       SET "name" = 'RF 5 Voter' 
       WHERE "id" = '0x41513aa7b99bfea09d389c74aacedaeb13c28fb748569e9e2400109cbe284ee5' 
-      AND "name" = ''RF 5 or 6 Voter';
+      AND "name" = 'RF 5 or 6 Voter';
     `);
   }
 };
