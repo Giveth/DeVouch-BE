143c6a0f2fe76303a06b69035d8d5cb9b727468f
Cherik <Pourcheriki@gmail.com>
2024-11-05 11:24:06 +0330

    init getOrganisationUserVouchCountBySource

--- src/server-extension/organization-resolver.ts
+++ src/server-extension/organization-resolver.ts
@@ -1,6 +1,11 @@
 import { Arg, Query, Resolver } from "type-graphql";
 import type { EntityManager } from "typeorm";
-import { VouchCountPerMonth, VouchCountResult } from "./types";
+import {
+  VouchCountByUser,
+  VouchCountByUserResult,
+  VouchCountPerMonth,
+  VouchCountResult,
+} from "./types";
 import { isValidDate } from "./helper";
 
 @Resolver()
@@ -82,6 +87,71 @@ export class OrganisationResolver {
     } catch (error: any) {
       console.error("Error fetching vouch count by date:", error);
       throw new Error(`Failed to fetch vouch count by date: ${error.message}`);
+    }
+  }
+
+  @Query(() => VouchCountByUserResult)
+  async getOrganisationUserVouchCountBySource(
+    @Arg("organisationId", () => String) organisationId: string,
+    @Arg("source", () => String) source: string,
+    @Arg("fromDate", () => String) fromDate: string,
+    @Arg("toDate", () => String) toDate: string
+  ): Promise<VouchCountByUserResult> {
+    // Validate date format
+    if (!isValidDate(fromDate) || !isValidDate(toDate)) {
+      throw new Error(
+        "Invalid date format. Dates must be in YYYY-MM-DD format."
+      );
+    }
+
+    const from = new Date(fromDate);
+    const to = new Date(toDate);
+    if (from > to) {
+      throw new Error("`fromDate` cannot be later than `toDate`.");
+    }
+
+    try {
+      const manager = await this.tx();
+
+      const query = `
+        SELECT 
+          attestor_organisation.attestor_id AS attestor_id,
+          COUNT(*) AS total_count
+        FROM 
+          project_attestation
+        JOIN attestor_organisation 
+          ON project_attestation.attestor_organisation_id = attestor_organisation.id
+        JOIN project
+          ON project_attestation.project_id = project.id
+        WHERE 
+          attestor_organisation.organisation_id = $1
+          AND project.source = $2
+          AND project_attestation.vouch = true
+          AND project_attestation.attest_timestamp BETWEEN $3 AND $4
+        GROUP BY 
+          attestor_organisation.attestor_id
+        ORDER BY 
+          total_count DESC;
+      `;
+
+      const params = [organisationId, source, fromDate, toDate];
+
+      const result = await manager.query(query, params);
+
+      const vouchCountByUser: VouchCountByUser[] = result.map((row: any) => ({
+        attestorId: row.attestor_id,
+        totalCount: Number(row.total_count),
+      }));
+
+      return {
+        totalUsers: vouchCountByUser.length,
+        vouchCountByUser,
+      };
+    } catch (error: any) {
+      console.error("Error fetching attestor vouch count by source:", error);
+      throw new Error(
+        `Failed to fetch attestor vouch count by source: ${error.message}`
+      );
     }
   }
 }
